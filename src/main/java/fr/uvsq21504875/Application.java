565a8b94005fb1b2b49c578d4f1e08591dea727a
Elarif Hamidou <elarif.hamidou@ens.uvsq.fr>
2020-03-26 16:51:42 +0100

    Début projet + Singleton + Checkstyle

---
+++ src/main/java/fr/uvsq21504875/Application.java
@@ -0,0 +1,13 @@
+package fr.uvsq21504875;
+
+public enum Application {
+  ENVIRONNEMENT;
+
+  public void run(String[] args){
+    //
+  }
+
+  public static void main(String[] args){
+    ENVIRONNEMENT.run(args);
+  }
+}
